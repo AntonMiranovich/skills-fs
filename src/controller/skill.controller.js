fffb8d10598c459b939243cff7c4d3ad701113fc
AntonMiranovich <antonmir992@gmail.com>
2023-11-02 22:39:08 +0300

    work midlle

---
+++ src/controller/skill.controller.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { getAllSkills, createSkills, updateSkills, deleteSkills } = require('../service/skill.service');
+const { isValidSkillsId,isValidSkillsData } = require('../helper/validation');
+const {bildResponse}=require('../helper/buildResponse')
+
+const route = express.Router();
+
+route.get('/', (req, res) => {
+  try {
+    bildResponse(res,200,getAllSkills())
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+route.post('/',isValidSkillsData, (req, res) => {
+  try {
+    const { title } = req.body;
+    bildResponse(res,200,createSkills(title))
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+route.put('/:id', isValidSkillsId,isValidSkillsData, (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+    bildResponse(res,200,updateSkills(id, title))
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+route.delete('/:id', isValidSkillsId, (req, res) => {
+  try {
+    const { id } = req.params;
+    bildResponse(res,200,deleteSkills(id))
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+module.exports = route;
---
+++ src/helper/validation.js
@@ -0,0 +1,15 @@
+function isValidSkillsId(req, res, next) {
+  const { id } = req.params;
+  if (isNaN(id)) throw new Error('ID должен быть числом');
+  if (id < 1) throw new Error('ID не может быть отрицательным');
+  next();
+}
+
+function isValidSkillsData(req, res, next) {
+    const { title } = req.body;
+    if (!isNaN(id)) throw new Error('title ту может быть числом');
+    if (!title) throw new Error('значени title пустое');
+    next();
+  }
+
+module.exports = { isValidSkillsId,isValidSkillsData };
